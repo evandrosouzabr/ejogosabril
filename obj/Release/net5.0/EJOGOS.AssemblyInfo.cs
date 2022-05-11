8f77ef10a2781ad5599252f010b742927e03e721
Evandro Vidéo de Souza Júnior <evandrosouzabr@gmail.com>
2022-05-11 15:32:04 -0400

    Atualização do ejogosabril
    Atualização do ejogosabril

--- obj/Release/net5.0/EJOGOS.AssemblyInfo.cs
+++ obj/Release/net5.0/EJOGOS.AssemblyInfo.cs
@@ -11,14 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ProvideApplicationPartFactoryAttribute("Microsoft.AspNetCore.Mvc.ApplicationParts.CompiledRazorAssemblyApplicationPartFac" +
-    "tory, Microsoft.AspNetCore.Mvc.Razor")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("EJOGOS")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyProductAttribute("EJOGOS")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyTitleAttribute("EJOGOS.Views")]
+[assembly: System.Reflection.AssemblyProductAttribute("EJOGOS")]
+[assembly: System.Reflection.AssemblyTitleAttribute("EJOGOS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
